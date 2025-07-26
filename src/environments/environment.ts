5e78a10cb269134e09382ee9e2851af15561c8d9
mohanademad643 <mohannad.emad535@gmail.com>
2025-07-26 22:16:04 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
   // apiBaseUrl: 'http://localhost:3000'
   // apiBaseUrl: 'https://denim-cloudy-ball.glitch.me'
   //  apiBaseUrl:'https://meadow-typical-robin.glitch.me'
-  apiBaseUrl:'https://flax-marmalade-walker.glitch.me'
+    apiBaseUrl:'https://70e14ffe-bb5a-4565-87a4-e4da9636eee3-00-3rw3h4zyw3af.worf.replit.dev'
+  // apiBaseUrl:'https://flax-marmalade-walker.glitch.me'
 };
